cb7eb1cc8a6ca2cc08862f09c429ec4f1f7363e9
tacticalDevC <anonymous@debian>
2018-11-22 10:47:27 +0100

    Initial commit

---
+++ app/src/main/java/at/tacticaldevc/panictrigger/TriggerActivity.java
@@ -0,0 +1,13 @@
+package at.tacticaldevc.panictrigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TriggerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trigger);
+    }
+}
