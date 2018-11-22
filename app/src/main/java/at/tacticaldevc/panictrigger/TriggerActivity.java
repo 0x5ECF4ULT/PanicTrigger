bf922316ac2175405644c98909a7e62db72f36bc
tacticalDevC <tacticalDevC@users.noreply.github.com>
2018-11-22 11:24:27 +0100

    TriggerActivity configured!

--- app/src/main/java/at/tacticaldevc/panictrigger/TriggerActivity.java
+++ app/src/main/java/at/tacticaldevc/panictrigger/TriggerActivity.java
@@ -2,12 +2,27 @@ package at.tacticaldevc.panictrigger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TriggerActivity extends AppCompatActivity {
+public class TriggerActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trigger);
+
+        findViewById(R.id.triggerButton).setOnLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        return false;
     }
 }
