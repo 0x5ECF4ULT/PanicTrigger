46f8d87dbb23551c44bd23d1a5c1bce50408b351
tacticalDevC <tacticalDevC@users.noreply.github.com>
2018-11-23 11:26:08 +0100

    triggerAlarm() implemented and TriggerActivity requests permissions now (forgot that :D). Slightly updated manifest

--- app/src/main/java/at/tacticaldevc/panictrigger/SMSListener.java
+++ app/src/main/java/at/tacticaldevc/panictrigger/SMSListener.java
@@ -3,11 +3,17 @@ package at.tacticaldevc.panictrigger;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class SMSListener extends BroadcastReceiver {
 
@@ -21,14 +27,30 @@ public class SMSListener extends BroadcastReceiver {
             {
                 if(contacts.contains(msg.getOriginatingAddress()) && msgs.contains(msg.getMessageBody()))
                 {
-                    triggerAlarm();
+                    triggerAlarm(context, msg.getOriginatingAddress());
                     break;
                 }
             }
         }
     }
 
-    private void triggerAlarm() {
+    private void triggerAlarm(Context context, String address) {
+        AudioManager audioManager = ((AudioManager) context.getSystemService(Context.AUDIO_SERVICE));
+        MediaPlayer mp = new MediaPlayer();
 
+        audioManager.setStreamVolume(AudioManager.STREAM_ALARM, audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM), 0);
+        try {
+            mp.setDataSource(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
+            mp.setLooping(true);
+            mp.prepare();
+            mp.start();
+            TimeUnit.MINUTES.sleep(1);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        mp.stop();
+        mp.release();
+
+        context.startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(address)));
     }
 }
--- app/src/main/java/at/tacticaldevc/panictrigger/TriggerActivity.java
+++ app/src/main/java/at/tacticaldevc/panictrigger/TriggerActivity.java
@@ -1,6 +1,8 @@
 package at.tacticaldevc.panictrigger;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,21 @@ public class TriggerActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trigger);
+
+        if(checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(new String[]{
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CALL_PHONE
+            }, 1);
+        }
     }
 
     @Override
